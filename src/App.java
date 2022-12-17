9dbd673c793c84b12c87ca3b4c8ebace498dd52d
nicolasmanurung <nicolas.manurung@dana.id>
2022-12-17 20:08:25 +0700

    Create ToDo list for task

--- src/App.java
+++ src/App.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 
 public class App {
+    // TODO("create variabel for Name, NIM, and numberInput")
+
+    private final int MAX_LENGTH = 25;
+
+    private final int MIN_LENGTH = 5;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Masukkan Sembarang Angka [5...20] : ");
         int batas = scanner.nextInt();
-        int past = 0, current = 1, fibonacci = 0, angka;
+        int past = 0, current = 1, fibonacci, angka;
         int sumGanjil = 0, sumGenap = 0, sumFibonacci = 0;
         
         System.out.println(batas + " Bilangan Genap");
@@ -41,5 +47,13 @@ public class App {
         }
         System.out.printf("\nHasil Penjumlahan = "+sumFibonacci);
 
-    } 
+    }
+
+    //TODO("create function to validate name length")
+
+    //TODO("create function to validate nim length")
+
+    //TODO("create function to validate inputNumber max size")
+
+    //TODO("Refactor code from main to 1 function, we could create showResult function, etc")
 }
